6d4eef359eb4fa3ce3dcbcab0a7f79ed48f132e1
tomaszczart <czart.tomasz@gmail.com>
2017-01-07 00:10:37 +0100

    Authorisation based on additional field in token.

--- api/routes/get/transactions.js
+++ api/routes/get/transactions.js
@@ -13,10 +13,9 @@ module.exports = (function() {
 
     router.get('/', function(req, res) {
         let token = req.headers['x-auth'];
-        let auth = jwt.decode(token, secretKey); // TODO username except email !!! (NO USERNAME FIELD IN DATABASE)
+        let auth = jwt.decode(token, secretKey);
         console.log(auth);
-        verifyUser(auth.email, isEmployee => {//TODO USERNAME
-            if(isEmployee){
+            if(auth.employee){
                 sql.query`SELECT * FROM Transactions`.then(recordset => {
                     res.json(recordset);
                 }).catch(err => {
@@ -25,20 +24,6 @@ module.exports = (function() {
             }else{
                 res.status(401).send();
             }
-        });
     });
     return router;
 })();
-
-/* Check if given user got permission to access this data */
-function verifyUser(email, callback){
-    sql.query`SELECT role FROM Customer WHERE email=${email}`.then(recordset => {//TODO USERNAME
-        if(recordset.length == 1 && recordset[0].role == 1){
-            callback(true);
-        }else{
-            callback(false);
-        }
-    }).catch(err => {
-        callback(false);
-    });
-}
