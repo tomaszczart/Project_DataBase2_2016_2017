c74598e94e1e34c9fb30136ebe5f1a089ba39384
tomaszczart <czart.tomasz@gmail.com>
2017-01-14 22:56:15 +0100

    Creating and ending transaction added.

---
+++ api/routes/post/endTransaction.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/**
+ * Created by Tomasz Czart on 27.12.2016.
+ */
+
+const router = require('express').Router();
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jwt-simple');
+const secretKey = require('../../js/secretKey').secretKey;
+const sql = require('../../db/db-connection');
+
+module.exports = (function() {
+
+    router.post('/', function(req, res) {
+        let token = req.headers['x-auth'];
+        let auth;
+
+        try{
+            auth = jwt.decode(token, secretKey);
+            if(auth.employee){
+                let transactionId = req.body.transactionId;
+                let dataEnd = req.body.dataEnd;
+                let price = req.body.price;
+
+                let request = new sql.Request();//end save in database
+                request.query(`UPDATE Transactions SET data_end='${dataEnd}', price=${price} WHERE transaction_id = ${transactionId}`).then(recordset => {
+                    res.status(201).send();//if everything goes right, return 201 status code
+                }).catch(err => {
+                    console.log(err)
+                    res.status(400).send();//else bad request code
+                });
+            }else{
+                res.status(401).send();
+            }
+        }catch(err){
+            res.status(401).send();
+        }
+    });
+    return router;
+})();
---
+++ api/routes/post/makeTransaction.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/**
+ * Created by Tomasz Czart on 27.12.2016.
+ */
+
+const router = require('express').Router();
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jwt-simple');
+const secretKey = require('../../js/secretKey').secretKey;
+const sql = require('../../db/db-connection');
+
+module.exports = (function() {
+
+    router.post('/', function(req, res) {
+        let token = req.headers['x-auth'];
+        let auth;
+
+        try{
+            auth = jwt.decode(token, secretKey);
+            if(auth.employee){
+                let customerId = req.body.customerId;
+                let employeeId = req.body.employeeId;
+                let dataStart = req.body.dataStart;
+
+                let request = new sql.Request();//end save in database
+                request.query(`INSERT INTO Transactions (customer_id, employee_id, data_start) VALUES (${customerId}, ${employeeId}, '${dataStart}')`).then(recordset => {
+                    res.status(201).send();//if everything goes right, return 201 status code
+                }).catch(err => {
+                    res.status(400).send();//else bad request code
+                });
+
+            }else{
+                res.status(401).send();
+            }
+        }catch(err){
+            res.status(401).send();
+        }
+    });
+    return router;
+})();
