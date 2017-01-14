750577eca0d5a217809e8518d0d70644f55685c3
tomaszczart <czart.tomasz@gmail.com>
2017-01-14 22:56:33 +0100

    Creating and ending transaction added.

--- api/app.js
+++ api/app.js
@@ -20,6 +20,8 @@ const getItemsByModel = require('./routes/get/itemsByModel');
 const postRegister = require('./routes/post/register');
 const postLogin = require('./routes/post/login');
 const postReservation = require('./routes/post/reservation');
+const postMakeTransaction = require('./routes/post/makeTransaction');
+const postEndTransaction = require('./routes/post/endTransaction');
 
 const PORT = 3000;
 
@@ -39,6 +41,8 @@ app.use('/get/itemsByModel', getItemsByModel);
 app.use('/post/register', postRegister);
 app.use('/post/login', postLogin);
 app.use('/post/reservation', postReservation);
+app.use('/post/makeTransaction', postMakeTransaction);
+app.use('/post/endTransaction', postEndTransaction);
 
 app.listen(PORT, () => {
     console.log(`Banch of Tools API is now running on port ${PORT}.`);
