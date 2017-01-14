db8573811af9f47f1b2b6a90e7100d617ed3abc1
tomaszczart <czart.tomasz@gmail.com>
2017-01-14 21:20:47 +0100

    Added itemsByModel and modelsByCategory.

---
+++ api/routes/get/itemsByModel.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Created by Tomasz Czart on 14.01.2017.
+ */
+
+const router = require('express').Router();
+const sql = require('../../db/db-connection');
+
+module.exports = (function() {
+
+    router.get('/:modelId', function(req, res) {
+
+        let modelId = req.params.modelId;
+
+        sql.query`select item_id, itemstatus, condition FROM Item WHERE model_id=${modelId}`.then(recordset => {
+            res.json(recordset);
+        }).catch(err => {
+            res.status(404).send();
+        });
+    });
+
+    return router;
+})();
---
+++ api/routes/get/modelsByCategory.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Created by Tomasz Czart on 14.01.2017.
+ */
+
+const router = require('express').Router();
+const sql = require('../../db/db-connection');
+
+module.exports = (function() {
+
+    router.get('/:categoryId', function(req, res) {
+
+        let categoryId = req.params.categoryId;
+
+        sql.query`select model_id, name, description, price_per_hour FROM Model WHERE category_id=${categoryId}`.then(recordset => {
+            res.json(recordset);
+        }).catch(err => {
+            res.status(404).send();
+        });
+    });
+
+    return router;
+})();
