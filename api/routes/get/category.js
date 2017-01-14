2617c522d94f3bd73ffd2cf014da995891381a3d
tomaszczart <czart.tomasz@gmail.com>
2017-01-14 20:46:45 +0100

    Get category added.

---
+++ api/routes/get/category.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * Created by Tomasz Czart on 11.11.2016.
+ */
+
+const router = require('express').Router();
+const sql = require('../../db/db-connection');
+
+module.exports = (function() {
+
+    router.get('/', function(req, res) {
+        sql.query`select * from Category`.then(recordset => {
+            res.json(recordset);
+        }).catch(err => {
+            res.status(404).send();
+        });
+    });
+    return router;
+})();
