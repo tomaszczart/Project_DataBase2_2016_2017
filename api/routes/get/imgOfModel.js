536abc331f4993fef10d69db1aa1cce171bc81c6
tomaszczart <czart.tomasz@gmail.com>
2017-01-14 19:51:51 +0100

    Separate pictures from rest model data.

--- api/routes/get/imgOfModel.js
+++ api/routes/get/imgOfModel.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**
- * Created by Tomasz Czart on 11.11.2016.
+ * Created by Tomasz Czart on 14.01.2017.
  */
 
 const router = require('express').Router();
@@ -9,8 +9,11 @@ const sql = require('../../db/db-connection');
 
 module.exports = (function() {
 
-    router.get('/', function(req, res) {
-        sql.query`SELECT model_id, name, description, price_per_hour, category_id FROM Model`.then(recordset => {
+    router.get('/:modelId', function(req, res) {
+
+        let modelId = req.params.modelId;
+
+        sql.query`select picture FROM Model WHERE model_id=${modelId}`.then(recordset => {
             res.json(recordset);
         }).catch(err => {
             res.status(404).send();
